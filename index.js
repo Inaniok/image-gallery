5a53d34fc628b92fc14a5a501849f532f792bf4a
Serhii Izotov <sergiyizotov@gmail.com>
2023-03-08 00:34:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var imageArray = [
-    "img/1.jpg", 
-    "img/2.jpg", 
-    "img/3.jpg", 
-    "img/4.jpg",
-    "img/5.jpg"
+    "./img/1.jpg", 
+    "./img/2.jpg", 
+    "./img/3.jpg", 
+    "./img/4.jpg",
+    "./img/5.jpg"
 ];
 
 window.number = '0';
